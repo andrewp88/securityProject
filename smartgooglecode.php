8977a9640c4b12b044450675235c034939c4507b
Andrew88Notebook <andriypolulyakh@gmail.com>
2019-08-25 17:19:23 +0200

    Added my python script request forger

---
+++ smartgooglecode.php
@@ -0,0 +1,6 @@
+<br />
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function add_action() in C:\xampp\htdocs\wordpress\wp-content\plugins\smart-google-code-inserter\smartgooglecode.php:20
+Stack trace:
+#0 C:\xampp\htdocs\wordpress\wp-content\plugins\smart-google-code-inserter\smartgooglecode.php(496): SmartGoogleCode-&gt;SmartGoogleCode()
+#1 {main}
+  thrown in <b>C:\xampp\htdocs\wordpress\wp-content\plugins\smart-google-code-inserter\smartgooglecode.php</b> on line <b>20</b><br />
